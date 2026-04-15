b3e34fcca28c7f02e30e4ad083f9ed42196f74b7
KottenAlin <156342596+KottenAlin@users.noreply.github.com>
2026-04-15 09:16:00 +0200

    test

--- Kod/del1.2.py
+++ Kod/del1.2.py
@@ -1,1 +1,16 @@
-def hellooworld
+# parameters
+c = 0.389
+S_t = 0.016
+K_1 = 0.249
+n_1 = 2.207
+
+# maximal pressure fall
+delta_p_max = 49080
+
+# Temperature difference
+delta_T = 29.6
+
+# power
+Q = 80136
+
+hej vi borde käa
